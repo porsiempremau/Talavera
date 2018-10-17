75b890d1f67126c7e096cead2459362a5c7f4797
TPUC <TPUC@VTCSIGSA>
2018-10-17 09:47:43 -0500

    Cambios HTML

--- TalaveraWeb/App_Start/BundleConfig.cs
+++ TalaveraWeb/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace TalaveraWeb
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/general.css"));
         }
     }
 }
